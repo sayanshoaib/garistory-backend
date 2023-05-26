4488e73f19b3075e660f8a2a27ce32dff7fa29f8
Shoaib Hasan <48613909+sayanshoaib@users.noreply.github.com>
2023-05-26 11:06:55 +0600

    FIX: db config

--- config/db_config.go
+++ config/db_config.go
@@ -2,25 +2,34 @@ package config
 
 import (
 	"database/sql"
+	"github.com/joho/godotenv"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
+	"log"
+	"os"
 )
 
-//type DbInstance struct {
-//	Db *bun.DB
-//}
-//
-//var DB DbInstance
-
 func ConnectDb() *bun.DB {
-	dsn := "postgres://shoaib:@localhost:5432/garistory?sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	//dsn := "postgres://" + os.Getenv("POSTGRES_USER") + ":" + os.Getenv("POSTGRES_PASSWORD") +
+	//	"@db/" + os.Getenv("POSTGRES_DATABASE") + "?sslmode=disable"
+	dsn := "postgres://" +
+		os.Getenv("POSTGRES_USER") + ":" +
+		os.Getenv("POSTGRES_PASSWORD") + "@" +
+		os.Getenv("POSTGRES_HOST") + ":" +
+		os.Getenv("POSTGRES_PORT") + "/" +
+		os.Getenv("POSTGRES_DB") + "?sslmode=disable"
+
 	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+	//dsn := "postgres://shoaib:@localhost:5432/garistory?sslmode=disable"
+	//sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
 
 	db := bun.NewDB(sqldb, pgdialect.New())
 
 	return db
-	//DB = DbInstance{
-	//	Db: db,
-	//}
 }
